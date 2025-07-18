b29c0681d05168efff233ac3a9bf65067ca5cf7e
Mohamad Faid Fadjri <121603763+faidfadjri@users.noreply.github.com>
2025-07-18 11:09:00 +0000

    feat: add UserRoleSeeder

--- app/Models/User/UserRole.php
+++ app/Models/User/UserRole.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserRole extends Model
 {
-    //
+    protected $table        = 'user_roles';
+    protected $primaryKey   = 'user_role_id';
+    public $timestamps      = true;
+    protected $guarded      = ['user_role_id'];
 }
